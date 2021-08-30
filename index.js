6bd1af8b2f41074738c14c79ca962ace4da56174
M-RAY47 <a.mray47@yahoo.fr>
2021-08-31 02:58:29 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,6 @@
+let elSubmit = document.getElementById('submit');
+console.log(elSubmit);
+
+function thanks(){
+	alert("Your survey has been submitted, thanks for your cooperation!!! stay tuned for more contents");
+}
